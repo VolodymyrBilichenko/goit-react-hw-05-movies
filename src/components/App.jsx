607a60aeaba4c48f1536a1885bf237fdc6ce355a
Volodymyr <61534586+VolodymyrBilichenko@users.noreply.github.com>
2024-02-03 14:47:58 +0200

    up

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { Route, Routes, Navigate } from "react-router-dom";
 import Layout from './layout/layout'
-import Home from "pages/homePage/homePage";
-import Movie from "pages/moviePage/movie";
-import MovieDetails from "pages/movieDetails/movieDetails";
+import Home from "../pages/homePage/homePage";
+import Movie from "../pages/moviePage/movie";
+import MovieDetails from "../pages/movieDetails/movieDetails";
 import Cast from "./cast/cast";
 import Reviews from "./reviews/reviews"
 
