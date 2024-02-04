99573b0b5a0c839283b72e8f080ce6cc702d5666
Volodymyr <61534586+VolodymyrBilichenko@users.noreply.github.com>
2024-02-04 16:02:21 +0200

    up pravki

--- src/components/cast/cast.jsx
+++ src/components/cast/cast.jsx
@@ -1,40 +1,38 @@
 import { useParams } from "react-router-dom";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import style from './coast.module.scss'
 import { getMovieDetailsCast } from '../../api/api';
 
-import { useEffect, useState } from "react";
-import style from './coast.module.scss'
-
 const Cast = () => {
-    
-    
-    const {movieid} = useParams()
+    const { movieid } = useParams();
+    const [castInfo, setCastInfo] = useState([]);
 
-    const [cast, setCast] = useState([]);
     useEffect(() => {
         const fetchData = async () => {
-          const res = await getMovieDetailsCast(movieid);
-          console.log(res.id);
-          setCast(res);
-       
+            const res = await getMovieDetailsCast(movieid);
+            console.log(res.id);
+            setCastInfo(res);
         };
         fetchData().catch(error => console.log(error));
-      }, [movieid]);
+    }, [movieid]);
 
-    return  <>
-    <ul className={style.list}>
-        {cast?.cast?.map(cast => {
-            return(
-                <li key={cast.id} className={style.item}>
-                    <img src={``} alt="" />
-                    <p>{cast.name}</p>
-                    <p>{cast.character}</p>
-                </li>
-            )
-        })}
-    </ul>
-    
-    </>
+    return (
+        <ul className={style.list}>
+            {castInfo?.cast?.length > 0 ? (
+                castInfo.cast.map(cast => (
+                    <li key={cast.id} className={style.item}>
+                        <img src={`https://image.tmdb.org/t/p/w500${cast.profile_path}`} alt="cast ph" />
+                        <div className={style.cast__name}>
+                            <p>{cast.name}</p>
+                            <p>{cast.character}</p>
+                        </div>
+                    </li>
+                ))
+            ) : (
+                <p>No cast information available</p>
+            )}
+        </ul>
+    );
 }
 
 export default Cast;
--- src/pages/moviePage/movie.jsx
+++ src/pages/moviePage/movie.jsx
@@ -2,64 +2,67 @@ import { useEffect, useState } from 'react';
 import { Link, useSearchParams, useLocation } from 'react-router-dom';
 import { getMovieName } from '../../api/api';
 import style from './movie.module.scss';
+
 const Movie = () => {
-  const location = useLocation()
+  const location = useLocation();
   const [searchParams, setSearchParams] = useSearchParams();
   const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
   const query = searchParams.get('query');
   console.log(query);
 
-  const updateQeryString = query => {
-  const nextParams = query !== '' ? {query} : {};
-  setSearchParams(nextParams)
-  }
+  const updateQueryString = (value) => {
+    const nextParams = value !== '' ? { query: value } : {};
+    setSearchParams(nextParams);
+  };
 
-  const handleInputChange = e => {
-      const newValue = e.target.value;
-      updateQeryString(newValue)
+  const handleInputChange = (e) => {
+    setSearchValue(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    updateQueryString(searchValue);
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await getMovieName(query);
+        console.log(res);
+        setMovies(res.results);
+      } catch (error) {
+        console.log(error);
+        // Обробити помилку запиту та вивести відповідне повідомлення користувачеві
+      }
     };
 
-
-    useEffect(() => {
-        const fetchData = async () => {
-          const res = await getMovieName(query);
-          console.log(res);
-          setMovies(res.results);
-         
-    
-        };
-    
-        fetchData().catch(error => console.log(error));
-      }, [query]);
-
+    if (query) {
+      fetchData().catch((error) => console.log(error));
+    }
+  }, [query]);
 
   return (
-    <>
+    <form onSubmit={handleSubmit}>
       <input
         className={style.input}
         type="text"
-        value={query || ''}
+        value={searchValue}
         onChange={handleInputChange}
       />
-      <button
-        type="button"
-        className={style.btn}
-        onClick={() => setSearchParams({ query })}
-      >
+      <button type="submit" className={style.btn}>
         search
       </button>
       <ul>
-        {movies.map(movie => {
-          return (
-            <li key={movie.id}>
-              <Link to={`/movies/${movie.id}`} state={{ from: location}}>
-                {movie.title}
-              </Link>
-            </li>
-          );
-        })}
+        {movies.map((movie) => (
+          <li key={movie.id}>
+            <Link to={`/movies/${movie.id}`} state={{ from: location }}>
+              {movie.title}
+            </Link>
+          </li>
+        ))}
       </ul>
-    </>
+    </form>
   );
 };
 
